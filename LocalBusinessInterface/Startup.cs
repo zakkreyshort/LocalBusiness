a345ee617aeb258f5861d5c43517bd96287af4df
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-04-03 16:34:28 -0700

    refactoring categories views, styling, startup

--- LocalBusinessInterface/Startup.cs
+++ LocalBusinessInterface/Startup.cs
@@ -21,12 +21,10 @@ namespace LocalBusinessInterface
 
     public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
       services.Configure<CookiePolicyOptions>(options =>
       {
-        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
         options.CheckConsentNeeded = context => true;
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
@@ -35,7 +33,6 @@ namespace LocalBusinessInterface
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
     }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
       if (env.IsDevelopment())
@@ -45,11 +42,9 @@ namespace LocalBusinessInterface
       else
       {
         app.UseExceptionHandler("/Home/Error");
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
 
-      // app.UseHttpsRedirection();
       app.UseStaticFiles();
       app.UseCookiePolicy();
 
