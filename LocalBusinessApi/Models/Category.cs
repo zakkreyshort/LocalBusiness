300498f9d8bb8952bb0e2d868b4df1a1c30dfffb
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-04-03 09:16:06 -0700

    create model for category and make link to place

---
+++ LocalBusinessApi/Models/Category.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace LocalBusinessApi.Models
+{
+  public class Category
+  {
+    public int CategoryId { get; set; }
+    [Required]
+    [StringLength(50)]
+    public string Name { get; set; }
+    [Required]
+    [StringLength(255)]
+    public string Description { get; set; }
+    public Category()
+    {
+      this.Places = new HashSet<Place>();
+    }
+  
+    public virtual ICollection<Place> Places { get; set; }
+  }
+    
+}
