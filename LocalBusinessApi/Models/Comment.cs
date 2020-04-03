7d80be04a45b5a872c24688cf54d9488aa499c2c
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-04-03 09:44:40 -0700

    create comment for places.

---
+++ LocalBusinessApi/Models/Comment.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+namespace LocalBusinessApi.Models
+{
+    public class Comment
+    {
+      public int CommentId { get; set; }
+      [Required]
+      [StringLength(255)]
+      public string Body { get; set; }
+      public int UserId { get; set; }
+      public int PlaceId { get; set; }
+      public DateTime DateCreated { get; set; }
+      public DateTime EditDate { get; set; }
+      public int Score { get; set; }
+    }
+}
