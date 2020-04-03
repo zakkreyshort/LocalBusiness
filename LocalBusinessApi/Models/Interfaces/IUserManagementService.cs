9d79850537bdd56950ca21ad2fcc691a7426712a
Zakkrey Short <zakkreyshort@Zakkreys-MacBook-Pro.local>
2020-04-03 13:51:46 -0700

    fixed category view to route correctly

--- LocalBusinessApi/Models/Interfaces/IUserManagementService.cs
+++ LocalBusinessApi/Models/Interfaces/IUserManagementService.cs
@@ -1,10 +1,7 @@
-// using System.Threading.Tasks;
-
 namespace LocalBusinessApi.Models
 {
   public interface IUserManagementService
   {
-    // Task<bool> IsValidUser(string userName, string password);
     bool IsValidUser(string username, string password);
   }
 }
